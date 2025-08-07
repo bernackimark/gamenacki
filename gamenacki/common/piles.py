2de5dd751382dbed2e90503f7e81796b2d7781da
bernackimark <bernackimark@gmail.com>
2025-08-06 20:04:19 -0400

    updated doc string

--- gamenacki/common/piles.py
+++ gamenacki/common/piles.py
@@ -1,4 +1,5 @@
-"""A module for general subclassers of cardnacki.pile.Pile ... not sure how valuable this is"""
+"""A module for general subclassers of cardnacki.pile.Pile ... not sure how valuable this is;
+The concept is that defining the Piles (core to most games) here will reduce the size of the GameState constructor"""
 
 from dataclasses import dataclass
 
